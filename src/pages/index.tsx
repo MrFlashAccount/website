2fa29e6e79109dafb9af9b48b4707b2f4746b638
Sergey Garin <60073410@leroymerlin.ru>
2021-03-11 23:39:00 +0300

    Add necesasy metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { CssBaseline, Link } from "../components";
 
 const IndexPage = () => {
   return (
     <>
+      <Helmet>
+        <html lang="en" itemType="https://schema.org/Person" itemScope />
+        <title>Sergey Garin</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑🏻‍💻</text></svg>"
+        />
+        <meta name="description" content="My contacts and cv" />
+        <link itemProp="url" href="https://garin.dev" />
+        <meta name="author" content="Sergey Garin" />
+        <meta property="og:title" content="Sergey Garin" />
+        <meta property="og:description" content="My contacts and cv" />
+        <meta property="og:locale" content="en" />
+        <meta property="og:site_name" content="Sergey Garin" />
+      </Helmet>
       <CssBaseline />
       <div
         role="presentation"
@@ -23,7 +39,8 @@ const IndexPage = () => {
             <h1 className="font-semibold md:text-5xl text-4xl text-gray-800 pt-8 pb-8">
               Hi! My name is{" "}
               <span role="presentation" className="text-blue-700">
-                Garin Sergey.
+                <span itemProp="familyName">Garin</span>{" "}
+                <span itemProp="givenName">Sergey</span>.
               </span>{" "}
               <br />
               I'm a frontend developer.
@@ -36,12 +53,34 @@ const IndexPage = () => {
             </h2>
             <div>
               <p className="text-xl">
-                Say <Link href="https://t.me/mrflashaccount">Hi!</Link> to me or
-                find me on the other platforms:{" "}
-                <Link href="https://twitter.com/mrflashaccount">twitter</Link>,{" "}
-                <Link href="https://github.com/MrFlashAccount">github</Link> or
-                you can write me a lovely letter:{" "}
-                <Link href="mailto:sergey@garin.dev">email</Link>
+                Say{" "}
+                <Link
+                  href="https://t.me/mrflashaccount"
+                  itemProp="sameAs"
+                  rel="me"
+                >
+                  Hi!
+                </Link>{" "}
+                to me or find me on the other platforms:{" "}
+                <Link
+                  href="https://twitter.com/mrflashaccount"
+                  itemProp="sameAs"
+                  rel="me"
+                >
+                  twitter
+                </Link>
+                ,{" "}
+                <Link
+                  href="https://github.com/MrFlashAccount"
+                  itemProp="sameAs"
+                  rel="me"
+                >
+                  github
+                </Link>{" "}
+                or you can write me a lovely letter:{" "}
+                <Link href="mailto:sergey@garin.dev" itemProp="email">
+                  email
+                </Link>
                 <br />
                 Peruse my{" "}
                 <Link href="https://hh.ru/resume/958a7f6aff022a7d620039ed1f647643674c50?print=true">
@@ -51,6 +90,9 @@ const IndexPage = () => {
             </div>
           </section>
         </main>
+        <footer className="block border-solid border-t border-blue-600 mt-10 pt-1">
+          Site is under construction 🚧
+        </footer>
       </div>
     </>
   );
