b6a770c00883d4bf74123fd02d5df041fb9e8b84
Sergey Garin <60073410@leroymerlin.ru>
2021-03-12 13:40:09 +0300

    Add favicon generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: "Sergey Garin's website",
   },
   plugins: [
-    "gatsby-plugin-sass",
+    `gatsby-plugin-sass`,
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
@@ -15,6 +15,17 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sergey Garin's website`,
+        short_name: `Sergey Garin's website`,
+        start_url: `/`,
+        background_color: `#fff`,
+        display: `standalone`,
+        icon: `src/images/favicon/icon.svg`,
+      },
     },
     "gatsby-plugin-no-javascript",
   ],
