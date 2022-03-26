1e7a88eb185a07b8709fadb64e565e76c90129ab
Sergey Garin <sergey.garin@cube.dev>
2022-03-26 18:12:48 +0400

    chore: migrate to astro

--- src/types/modules.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.module.scss" {
-  const styles: any;
-  module.exports = {} as Record<string, string>;
-}
-
-declare module "*.css" {
-  const styles: any;
-  module.exports = {} as Record<string, string>;
-}
