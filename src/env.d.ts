9f8018bb40d47cb296db2b8e901de3cf89b81264
Sergei Garin <sergei@garin.dev>
2025-10-19 12:55:41 +0300

    add jxl

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+
+declare module "*.jxl" {
+	const metadata: ImageMetadata;
+	export default metadata;
+}
